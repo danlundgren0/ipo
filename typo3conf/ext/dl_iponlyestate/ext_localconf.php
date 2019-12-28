ba7db6e38d81f3d920a45d4a020b75f3a5b6f343
Dan Lundgren <danlundgren0@gmail.com>
2019-12-28 19:35:52 +0100

    Punkt 2 Login cookie

--- typo3conf/ext/dl_iponlyestate/ext_localconf.php
+++ typo3conf/ext/dl_iponlyestate/ext_localconf.php
@@ -89,5 +89,8 @@ if (!defined('TYPO3_MODE')) {
 ## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:dl_iponlyestate/Classes/Hooks/copyPageTree.php:DanLundgren\DlIponlyestate\Hooks\injectCopyAndPaste';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = 'EXT:dl_iponlyestate/Classes/Hooks/copyPageTree.php:DanLundgren\DlIponlyestate\Hooks\injectCopyAndPaste';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['login_confirmed'][] = 'EXT:dl_iponlyestate/Classes/Hooks/SetLoginCookie.php:DanLundgren\DlIponlyestate\Hooks\SetLoginCookie->setLoginCookie';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['logout_confirmed'][] = 'EXT:dl_iponlyestate/Classes/Hooks/SetLoginCookie.php:DanLundgren\DlIponlyestate\Hooks\SetLoginCookie->delLoginCookie';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['felogin']['beforeRedirect'][] = 'EXT:dl_iponlyestate/Classes/Hooks/SetLoginCookie.php:DanLundgren\DlIponlyestate\Hooks\SetLoginCookie->delLoginCookie';
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('DanLundgren\\DlIponlyestate\\Property\\TypeConverter\\UploadedFileReferenceConverter');
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('DanLundgren\\DlIponlyestate\\Property\\TypeConverter\\ObjectStorageConverter');
